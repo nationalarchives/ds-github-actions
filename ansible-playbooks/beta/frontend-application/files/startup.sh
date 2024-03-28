ee6cadc56c07e112bda350da9333c0447173d2f1
karlkern <karl.kern@nationalarchives.gov.uk>
2024-03-28 10:15:13 +0000

    use templates; use linux 2;

--- ansible-playbooks/beta/frontend-application/files/startup.sh
+++ ansible-playbooks/beta/frontend-application/files/startup.sh
@@ -7,17 +7,17 @@ sudo touch /var/log/server-startup.log
 
 region="eu-west-2"
 if [ -z ${TRAEFIK_IMAGE+x} ]; then export TRAEFIK_IMAGE="none"; fi
-if [ -z ${WEBSERVER_IMAGE+x} ]; then export WEBSERVER_IMAGE="none"; fi
+if [ -z ${FRONTEND_APP_IMAGE+x} ]; then export FRONTEND_APP_IMAGE="none"; fi
 
 
 # get docker image tag from parameter store
 echo "retrieve versions"
-exp_traefik_image=$(aws ssm get-parameter --name /application/private_beta/dw/docker/release --query Parameter.Value --region $region --output text | jq -r '.["traefik"]')
-exp_webserver_image=$(aws ssm get-parameter --name /application/private_beta/dw/docker/release --query Parameter.Value --region $region --output text | jq -r '.["website"]')
+exp_traefik_image=$(aws ssm get-parameter --name /application/beta/docker-images --query Parameter.Value --region $region --output text | jq -r '.["traefik"]')
+exp_frontend_app_image=$(aws ssm get-parameter --name /application/betadocker-images --query Parameter.Value --region $region --output text | jq -r '.["frontend-application"]')
 
 # update compose.traefik.yml if needed
 if [ "$TRAEFIK_IMAGE" = "$exp_traefik_image" ]; then
-  sudo sed -i "s|image: traefik:.*|image: traefik:$exp_traefik_image|g" /var/docker/compose.traefik.yml
+  sudo sed -i "s|image: traefik:.*|image: $exp_traefik_image|g" /var/docker/compose.traefik.yml
   update_traefik="yes"
   echo "traefik image updating to $exp_traefik_image"
 else
@@ -42,10 +42,10 @@ else
 fi
 
 # update compose.yml if needed
-if [ "$WEBSERVER_IMAGE" = "$exp_webserver_image" ]; then
-  sudo sed -i "s|image: ghcr.io/nationalarchives/national-archives-website:.*|image: ghcr.io/nationalarchives/national-archives-website:$exp_webserver_image|g" /var/docker/compose.yml
+if [ "$FRONTEND_APP_IMAGE" = "$exp_frontend_app_image" ]; then
+  sudo sed -i "s|image:.*|image: $exp_frontend_app_image|g" /var/docker/compose.yml
   update_website="yes"
-  echo "website needs updating to $exp_webserver_image"
+  echo "website needs updating to $exp_frontend_app_image"
 else
   update_website="no"
 fi
@@ -55,17 +55,17 @@ BLUE_ID=$(sudo docker ps --filter "name=blue-dw" --format "{{.ID}}")
 GREEN_ID=$(sudo docker ps --filter "name=green-dw" --format "{{.ID}}")
 if [ -z "$BLUE_ID" ] && [ -z "$GREEN_ID" ]; then
   # website isn't running
-  echo "starting up website - $exp_webserver_image"
+  echo "starting up website - $exp_frontend_app_image"
   source /usr/local/bin/website-blue-green-deploy.sh
-  sudo sed -i "s|export WEBSERVER_IMAGE=.*|export WEBSERVER_IMAGE=$exp_webserver_image|g" /etc/environment
-  echo "webserver image version set to $exp_webserver_image"
+  sudo sed -i "s|export FRONTEND_APP_IMAGE=.*|export FRONTEND_APP_IMAGE=$exp_frontend_app_image|g" /etc/environment
+  echo "webserver image version set to $exp_frontend_app_image"
 elif [ $update_website = "yes" ]; then
-  echo "updating website to $exp_webserver_image ..."
+  echo "updating website to $exp_frontend_app_image ..."
   source /usr/local/bin/website-blue-green-deploy.sh
-  sudo sed -i "s|export WEBSERVER_IMAGE=.*|export WEBSERVER_IMAGE=$exp_webserver_image|g" /etc/environment
-  echo "webserver image version set to $exp_webserver_image"
+  sudo sed -i "s|export FRONTEND_APP_IMAGE=.*|export FRONTEND_APP_IMAGE=$exp_frontend_app_image|g" /etc/environment
+  echo "webserver image version set to $exp_frontend_app_image"
 else
-  echo "website is ok - tag:$exp_webserver_image"
+  echo "website is ok - tag:$exp_frontend_app_image"
 fi
 
 sudo source /etc/environment
