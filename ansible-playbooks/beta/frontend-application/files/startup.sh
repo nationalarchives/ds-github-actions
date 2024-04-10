b970c71639b163688f0dcefa28baa20c05d24ef1
karlkern <karl.kern@nationalarchives.gov.uk>
2024-04-10 13:19:21 +0100

    change yq call

--- ansible-playbooks/beta/frontend-application/files/startup.sh
+++ ansible-playbooks/beta/frontend-application/files/startup.sh
@@ -9,15 +9,17 @@ region="eu-west-2"
 if [ -z ${TRAEFIK_IMAGE+x} ]; then export TRAEFIK_IMAGE="none"; fi
 if [ -z ${FRONTEND_APP_IMAGE+x} ]; then export FRONTEND_APP_IMAGE="none"; fi
 
-
 # get docker image tag from parameter store
 echo "retrieve versions"
 exp_traefik_image=$(aws ssm get-parameter --name /application/beta/docker-images --query Parameter.Value --region $region --output text | jq -r '.["traefik"]')
-exp_app_image=$(aws ssm get-parameter --name /application/docker-images --query Parameter.Value --region $region --output text | jq -r '.["frontend-application"]')
+exp_app_image=$(aws ssm get-parameter --name /application/beta/docker-images --query Parameter.Value --region $region --output text | jq -r '.["frontend-application"]')
+
+set_traefik_image=$(yq '.services.traefik.image' /var/docker/compose.traefik.yml)
+set_app_image=$(yq '.services.blue-web.image' /var/docker/compose.yml)
 
 # update traefik version if needed
-if [ "$TRAEFIK_IMAGE" != "$exp_traefik_image" ]; then
-  yq '.services.traefik.image = "$exp_traefik_image"' /var/docker/compose.traefik.yml
+if [[ "$TRAEFIK_IMAGE" != "$exp_traefik_image" ]] || [[ "$set_traefik_image" != "$exp_traefik_image" ]]; then
+  sudo yq -i ".services.traefik.image = \"$exp_traefik_image)\"" /var/docker/compose.traefik.yml
   export TRAEFIK_IMAGE="$exp_traefik_image"
   sudo sed -i "s|export TRAEFIK_IMAGE=.*|export TRAEFIK_IMAGE=\"$exp_traefik_image\"|g" /etc/environment
 fi
@@ -30,9 +32,9 @@ fi
 sudo /usr/local/bin/traefik-run.sh
 
 # update app version
-if [ "$FRONTEND_APP_IMAGE" = "$exp_app_image" ]; then
-  yq '.services.blue-web.image = "$exp_app_image"' /var/docker/compose.yml
-  export $FRONTEND_APP_IMAGE="$exp_app_image"
+if [[ "$FRONTEND_APP_IMAGE" = "$exp_app_image" ]] || [[ "$set_app_image" != "$exp_app_image" ]]; then
+  sudo yq -i ".services.blue-web.image = \"$exp_app_image\"" /var/docker/compose.yml
+  export FRONTEND_APP_IMAGE="$exp_app_image"
   sudo sed -i "s|export FRONTEND_APP_IMAGE=.*|export FRONTEND_APP_IMAGE=\"$exp_app_image\"|g" /etc/environment
 fi
 
