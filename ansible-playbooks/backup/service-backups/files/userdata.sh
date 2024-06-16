1ce445632572e9a5654238f0da372d68139c0423
karlkern <karl.kern@nationalarchives.gov.uk>
2024-06-16 15:47:51 +0100

    python

--- ansible-playbooks/backup/service-backups/files/userdata.sh
+++ ansible-playbooks/backup/service-backups/files/userdata.sh
@@ -15,7 +15,7 @@ sudo /sbin/swapon /var/swap.1
 # Install Cloudwatch agent
 echo "$(date '+%Y-%m-%d %T') - install CloudWatch agent" | sudo tee -a  /var/log/ami-install.log > /dev/null
 sudo dnf install amazon-cloudwatch-agent -y
-sudo aws s3 cp s3://{{ s3_deployment_bucket }}/{{ s3_deployment_root }}/cloudwatch/cloudwatch-agent-config.json /opt/aws/amazon-cloudwatch-agent/bin/config.json
+sudo aws s3 cp s3://tna-backup-tooling/cloudwatch/cloudwatch-agent-config.json /opt/aws/amazon-cloudwatch-agent/bin/config.json
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json
 
 echo "$(date '+%Y-%m-%d %T') - install collectd" | sudo tee -a  /var/log/ami-install.log > /dev/null
@@ -23,6 +23,10 @@ sudo dnf install collectd -y
 
 echo "$(date '+%Y-%m-%d %T') - install git" | sudo tee -a  /var/log/ami-install.log > /dev/null
 sudo dnf install git -y
+
+echo "$(date '+%Y-%m-%d %T') - install python 3.11" | sudo tee -a  /var/log/ami-install.log > /dev/null
+sudo dnf install python3.11 -y
+sudo dnf install python3.11-pip -y
 
 cat << EOF > /var/finish-init.txt
 [status]
