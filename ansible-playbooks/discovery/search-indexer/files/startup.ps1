e41d333af57d03a514c0db92a7574eeaccba1fb6
Steven Hirschorn <steve.hirschorn@gmail.com>
2025-01-13 16:12:58 +0000

    Create build package for new search indexer

---
+++ ansible-playbooks/discovery/search-indexer/files/startup.ps1
@@ -0,0 +1,37 @@
+# startup and deployment script for search-indexer .NET framework
+
+$logFile = "\startup.log"
+$runFlag = "\startupActive.txt"
+$codeTarget = "c://search-index"
+
+function write-log
+{
+   param(
+        [string]$Message,
+        [string]$Severity = 'Information'
+   )
+
+   $Time = (Get-Date -f g)
+   Add-content $logFile -value "$Time - $Severity - $Message"
+}
+
+try {
+	if (Test-Path -Path "$runFlag" -PathType leaf) {
+		write-log -Message "script is already active" -Severity "Warning"
+		exit
+	} else {
+		$Time = (Get-Date -f g)
+		Add-content $runFlag -value "$Time - startup script is activated"
+	}
+
+	write-log -Message "update code from S3"
+	aws s3 cp s3://ds-staging-deployment-source/search-index/search-index.zip /temp/search-index.zip
+
+	write-log -Message "write code"
+	expand-archive -Path \temp\search-index.zip -DestinationPath \ -force
+} catch {
+	write-log -Message "Caught an exception:" -Severity "Error"
+	write-log -Message "Exception Type: $($_.Exception.GetType().FullName)" -Severity "Error"
+	write-log -Message "Exception Message: $($_.Exception.Message)" -Severity "Error"
+    exit 1
+}
