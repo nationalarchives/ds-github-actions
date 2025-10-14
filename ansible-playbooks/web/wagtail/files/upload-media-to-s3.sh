d3af9bf3c912e5e1f292f52c5417a288f93911b8
Vaishnavi Reddy Putluru <vaishnavireddyputluru@Vaishnavis-MacBook-Pro.local>
2025-10-14 12:03:34 +0100

    debug

--- ansible-playbooks/web/wagtail/files/upload-media-to-s3.sh
+++ ansible-playbooks/web/wagtail/files/upload-media-to-s3.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Configurations
 SOURCE_DIR="/media"
 TMP_DIR="/media/.backup-zips"
 S3_BUCKET="s3://ds-dev-deployment-source/wagtail-content"
@@ -9,17 +7,11 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 ZIP_NAME="media-backup-$TIMESTAMP.zip"
 ZIP_PATH="$TMP_DIR/$ZIP_NAME"
 
-# Setup
 sudo mkdir -p "$TMP_DIR"
 sudo mkdir -p /var/log
 
-# Find updated files in the last 24h excluding wagtail-content.zip
-UPDATED_FILES=$(find "$SOURCE_DIR" -type f -mtime -1 ! -name "wagtail-content.zip")
-
-if [ -z "$UPDATED_FILES" ]; then
-    echo "$(date): No updated files to backup." >> "$LOG_FILE"
-    exit 0
-fi
+# Find all files excluding wagtail-content.zip
+UPDATED_FILES=$(find "$SOURCE_DIR" -type f ! -name "wagtail-content.zip")
 
 # Create zip
 cd "$SOURCE_DIR"
@@ -29,7 +21,7 @@ echo "$UPDATED_FILES" | zip -@ "$ZIP_PATH"
 sudo aws s3 cp "$ZIP_PATH" "$S3_BUCKET/$ZIP_NAME"
 
 # Log the operation
-sudo echo "$(date): Uploaded $ZIP_PATH to $S3_BUCKET/$ZIP_NAME" >> "$LOG_FILE"
+echo "$(date): Uploaded full backup $ZIP_PATH to $S3_BUCKET/$ZIP_NAME" | sudo tee -a "$LOG_FILE"
 
 # Clean up
 sudo rm -f "$ZIP_PATH"
